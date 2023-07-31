bed07eff420f3ee88210a3eacc4626911a359674
James Hannah <james.hannah@stfc.ac.uk>
2023-07-31 12:00:09 +0100

    Morphism

--- src/bin/mr-nag.rs
+++ src/bin/mr-nag.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use chrono::{DateTime, Local, Utc};
 use clap::Parser;
 use gitlab::{
     api::{
@@ -9,7 +10,10 @@ use gitlab::{
 };
 use reqwest::Url;
 use secrecy::{ExposeSecret, SecretString};
-use slack_hook::{AttachmentBuilder, PayloadBuilder, Slack};
+use slack_morphism::{
+    prelude::{SlackApiPostWebhookMessageRequest, SlackClientHyperConnector},
+    slack_blocks, SlackClient, SlackMessageContent,
+};
 
 /// Notifier to send a Slack Webhook if open Merge Requests exist for a
 /// particular Gitlab project.
@@ -42,9 +46,14 @@ struct CmdArgs {
     /// target of this specific branch will trigger the notification.
     #[arg(short = 'T', long, env)]
     target_branch: Option<String>,
+    /// Specify a minimum time which an MR must be "idle" for before creating a
+    /// notification. Note that this may cause notifications to be missed if the
+    /// execution interval is insufficient.
+    #[arg(short, long, env)]
+    min_dwell_secs: Option<i64>,
 }
 
-fn open_mr_list_url_for(project: &Project, target_branch: &Option<String>) -> Url {
+fn mr_list_url_for(project: &Project, target_branch: &Option<String>) -> Url {
     // We have to add a trailing slash to the URL or Url::parse will strip off
     // the last component from the URL
     let pwurl = format!("{}/", &project.web_url);
@@ -61,11 +70,10 @@ fn open_mr_list_url_for(project: &Project, target_branch: &Option<String>) -> Ur
     base
 }
 
-fn main() -> anyhow::Result<()> {
-    let args = CmdArgs::parse();
-    let gitlab = Gitlab::builder(args.gitlab_host, args.gitlab_token.expose_secret())
-        .build()
-        .context("unable to build gitlab API client")?;
+fn get_mrs<T>(args: &CmdArgs, gitlab: &Gitlab) -> anyhow::Result<T>
+where
+    T: Iterator<Item = MergeRequest>,
+{
     let mut mr_q_b = merge_requests::MergeRequests::builder();
     mr_q_b
         .project(args.gitlab_project_id)
@@ -74,18 +82,32 @@ fn main() -> anyhow::Result<()> {
         mr_q_b.target_branch(target_branch);
     }
     let mr_q = mr_q_b.build()?;
-    let mrs: Vec<MergeRequest> = mr_q.query(&gitlab).unwrap();
-    if let Some(first_mr) = mrs.first() {
+    let mrs: Vec<MergeRequest> = mr_q.query(gitlab).unwrap();
+    let now = Local::now();
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = CmdArgs::parse();
+    let gitlab = Gitlab::builder(args.gitlab_host, args.gitlab_token.expose_secret())
+        .build()
+        .context("unable to build gitlab API client")?;
+    let mrs = get_mrs(&args, &gitlab).unwrap();
+    for mr in mrs {
         // We are only pulling the first here, but we only really need to notify
         // if there is at least one MR waiting, any others don't really matter.
         let target_project_query = projects::Project::builder()
-            .project(first_mr.project_id.value())
+            .project(mr.project_id.value())
             .build()?;
+        if let Some(dwell) = args.min_dwell_secs {
+            if now.signed_duration_since(mr.updated_at).num_seconds() > dwell {
+                continue;
+            }
+        }
         let project: Project = target_project_query.query(&gitlab)?;
-        let mr_list_url = open_mr_list_url_for(&project, &args.target_branch);
+        let mr_list_url = mr_list_url_for(&project, &args.target_branch);
         let msg = format!(
-            "{} MR is awaiting merge{}",
-            mrs.len(),
+            "MR is awaiting merge{}",
             match &args.target_branch {
                 None => ".".to_string(),
                 Some(tb) => format!(" to target branch: {}.", &tb),
@@ -94,27 +116,36 @@ fn main() -> anyhow::Result<()> {
         // print the message to stdout
         println!("{msg}");
         if let Some(hook_url) = &args.slack_webhook_url {
-            let hook = Slack::new(hook_url.as_str()).unwrap();
-            // Create a Slack "attachment" that will give a bit more info than the text
-            let msg_attachment = AttachmentBuilder::new(msg)
-                .title(format!("Open Merge requests for {}", project.name))
-                .title_link(mr_list_url.as_str())
-                .footer("These should be merged before the end of the day if possible!")
-                .build()
-                .unwrap();
-            // Create the message and stick the attachment on it
-            let payload = PayloadBuilder::new()
-                .username("Gitlab Merge Request Nagbot")
-                .icon_emoji(":pencil:")
-                .attachments(vec![msg_attachment])
-                .build()
-                .unwrap();
-            // Send the message. We can just panic if it fails because we're about to exit anyway.
-            hook.send(&payload).unwrap();
+            let client = SlackClient::new(SlackClientHyperConnector::new());
+            client
+                .post_webhook_message(
+                    hook_url,
+                    &SlackApiPostWebhookMessageRequest::new(
+                        SlackMessageContent::new()
+                            .with_text(msg)
+                            .with_blocks(slack_blocks![]),
+                    ),
+                )
+                .await
+                .context("failed to send webhook")?;
+            // let hook = Slack::new(hook_url.as_str()).unwrap();
+            // // Create a Slack "attachment" that will give a bit more info than the text
+            // let msg_attachment = AttachmentBuilder::new(msg)
+            //     .title(format!("Open Merge requests for {}", project.name))
+            //     .title_link(mr_list_url.as_str())
+            //     .footer("These should be merged before the end of the day if possible!")
+            //     .build()
+            //     .unwrap();
+            // // Create the message and stick the attachment on it
+            // let payload = PayloadBuilder::new()
+            //     .username("Gitlab Merge Request Nagbot")
+            //     .icon_emoji(":pencil:")
+            //     .attachments(vec![msg_attachment])
+            //     .build()
+            //     .unwrap();
+            // // Send the message. We can just panic if it fails because we're about to exit anyway.
+            // hook.send(&payload).unwrap();
         }
-    } else {
-        // Nothing to do here
-        return Ok(());
     }
     Ok(())
 }
