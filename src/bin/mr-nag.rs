dc439e8ad0cdc8375f9e8156bc1d9c4bb4dee010
James Hannah <james.hannah@stfc.ac.uk>
2023-07-31 13:57:12 +0100

    Switch to slack_morphism crate

--- src/bin/mr-nag.rs
+++ src/bin/mr-nag.rs
@@ -1,12 +1,9 @@
 use anyhow::Context;
-use chrono::{DateTime, Local, Utc};
+use chrono::Local;
 use clap::Parser;
 use gitlab::{
-    api::{
-        projects::{self, merge_requests},
-        Query,
-    },
-    Gitlab, MergeRequest, Project,
+    api::{projects::merge_requests, AsyncQuery, Query},
+    AsyncGitlab, Gitlab, MergeRequest,
 };
 use reqwest::Url;
 use secrecy::{ExposeSecret, SecretString};
@@ -49,63 +46,42 @@ struct CmdArgs {
     /// Specify a minimum time which an MR must be "idle" for before creating a
     /// notification. Note that this may cause notifications to be missed if the
     /// execution interval is insufficient.
-    #[arg(short, long, env)]
+    #[arg(short = 'd', long, env)]
     min_dwell_secs: Option<i64>,
 }
 
-fn mr_list_url_for(project: &Project, target_branch: &Option<String>) -> Url {
-    // We have to add a trailing slash to the URL or Url::parse will strip off
-    // the last component from the URL
-    let pwurl = format!("{}/", &project.web_url);
-    let mut base = Url::parse(&pwurl).unwrap();
-    base = base.join("-/merge_requests/").unwrap();
-    // Set the URL query to filter for open ones
-    let mut mr_query = String::from("?scope=all&state=opened");
-    if let Some(tb) = target_branch {
-        // add the filter for the specified target branch
-        mr_query.push_str("&target_branch=");
-        mr_query.push_str(tb);
-    }
-    base.set_query(Some(&mr_query));
-    base
-}
-
-fn get_mrs<T>(args: &CmdArgs, gitlab: &Gitlab) -> anyhow::Result<T>
-where
-    T: Iterator<Item = MergeRequest>,
-{
-    let mut mr_q_b = merge_requests::MergeRequests::builder();
-    mr_q_b
+async fn get_mrs<'a>(
+    args: &CmdArgs,
+    gitlab: &'a AsyncGitlab,
+) -> anyhow::Result<impl IntoIterator<Item = MergeRequest>> {
+    let tb = args.target_branch.as_ref().map_or("", |x| &x);
+    let mr_q = merge_requests::MergeRequests::builder()
         .project(args.gitlab_project_id)
-        .state(merge_requests::MergeRequestState::Opened);
-    if let Some(target_branch) = &args.target_branch {
-        mr_q_b.target_branch(target_branch);
-    }
-    let mr_q = mr_q_b.build()?;
-    let mrs: Vec<MergeRequest> = mr_q.query(gitlab).unwrap();
-    let now = Local::now();
+        .state(merge_requests::MergeRequestState::Opened)
+        .target_branch(tb)
+        .build()
+        .unwrap();
+    //  mr_q_b = *mr_q_b.target_branch(target_branch);
+    // }
+    // let mr_q = mr_q_b.build()?;
+    let mrs: Vec<MergeRequest> = mr_q.query_async(gitlab).await.unwrap();
+    Ok(mrs)
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = CmdArgs::parse();
-    let gitlab = Gitlab::builder(args.gitlab_host, args.gitlab_token.expose_secret())
-        .build()
+    let gitlab = Gitlab::builder(&args.gitlab_host, args.gitlab_token.expose_secret())
+        .build_async()
+        .await
         .context("unable to build gitlab API client")?;
-    let mrs = get_mrs(&args, &gitlab).unwrap();
-    for mr in mrs {
-        // We are only pulling the first here, but we only really need to notify
-        // if there is at least one MR waiting, any others don't really matter.
-        let target_project_query = projects::Project::builder()
-            .project(mr.project_id.value())
-            .build()?;
+    let now = Local::now();
+    for mr in get_mrs(&args, &gitlab).await.unwrap() {
         if let Some(dwell) = args.min_dwell_secs {
             if now.signed_duration_since(mr.updated_at).num_seconds() > dwell {
                 continue;
             }
         }
-        let project: Project = target_project_query.query(&gitlab)?;
-        let mr_list_url = mr_list_url_for(&project, &args.target_branch);
         let msg = format!(
             "MR is awaiting merge{}",
             match &args.target_branch {
@@ -128,23 +104,6 @@ async fn main() -> anyhow::Result<()> {
                 )
                 .await
                 .context("failed to send webhook")?;
-            // let hook = Slack::new(hook_url.as_str()).unwrap();
-            // // Create a Slack "attachment" that will give a bit more info than the text
-            // let msg_attachment = AttachmentBuilder::new(msg)
-            //     .title(format!("Open Merge requests for {}", project.name))
-            //     .title_link(mr_list_url.as_str())
-            //     .footer("These should be merged before the end of the day if possible!")
-            //     .build()
-            //     .unwrap();
-            // // Create the message and stick the attachment on it
-            // let payload = PayloadBuilder::new()
-            //     .username("Gitlab Merge Request Nagbot")
-            //     .icon_emoji(":pencil:")
-            //     .attachments(vec![msg_attachment])
-            //     .build()
-            //     .unwrap();
-            // // Send the message. We can just panic if it fails because we're about to exit anyway.
-            // hook.send(&payload).unwrap();
         }
     }
     Ok(())
