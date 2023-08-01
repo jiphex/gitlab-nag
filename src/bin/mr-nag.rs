5bfb4841f43eef5fcd14b938e9239bd592becced
James Hannah <james.hannah@stfc.ac.uk>
2023-08-01 23:58:13 +0100

    various

--- src/bin/mr-nag.rs
+++ src/bin/mr-nag.rs
@@ -1,3 +1,4 @@
+
 use anyhow::Context;
 use chrono::Local;
 use clap::Parser;
@@ -8,6 +9,8 @@ use gitlab::{
 use reqwest::Url;
 use secrecy::{ExposeSecret, SecretString};
 use slack_morphism::prelude::*;
+use tracing::{debug,info_span, trace};
+use tracing_subscriber::{prelude::*, EnvFilter};
 
 /// Notifier to send a Slack Webhook if open Merge Requests exist for a
 /// particular Gitlab project.
@@ -45,22 +48,36 @@ struct CmdArgs {
     /// execution interval is insufficient.
     #[arg(short = 'd', long, env)]
     min_dwell_secs: Option<i64>,
+    /// Enable verbose logging
+    #[arg(short = 'v', long, default_value_t = false)]
+    verbose: bool,
 }
 
 /// Get the merge requests as per the input args, filtering for project, state (open) and target branch (if specified)
+#[tracing::instrument(skip_all)]
 async fn get_mrs<'a>(
     args: &CmdArgs,
     gitlab: &'a AsyncGitlab,
 ) -> anyhow::Result<impl IntoIterator<Item = MergeRequest>> {
-    let tb = args.target_branch.as_ref().map_or("main", |x| &x);
+    let tb = args.target_branch.as_ref().map_or("", |x| &x);
+    debug!(
+        target_branch = tb,
+        project_id = args.gitlab_project_id,
+        "searching project for open MRs"
+    );
     let mr_q = merge_requests::MergeRequests::builder()
         .project(args.gitlab_project_id)
         .state(merge_requests::MergeRequestState::Opened)
+        .wip(false)
         .target_branch(tb)
         .build()
         .unwrap();
     // have to use let ... here to explicitly inform the type (Vec)
     let merge_requests: Vec<MergeRequest> = mr_q.query_async(gitlab).await.unwrap();
+    debug!(
+        mr_in_scope = merge_requests.len(),
+        "merge request query complete"
+    );
     Ok(merge_requests)
 }
 
@@ -68,22 +85,65 @@ struct WrappedMR(MergeRequest);
 
 impl SlackMessageTemplate for WrappedMR {
     fn render_template(&self) -> SlackMessageContent {
-        todo!()
+        // self.0.u
+        SlackMessageContent::new()
+            .with_text(format!(
+                "MR #{} awaiting merge: \"{}\"",
+                self.0.id, self.0.title
+            ))
+            .with_blocks(slack_blocks![
+                some_into(SlackHeaderBlock::new(md!(format!(
+                    "MR **#{}: [{}]({})**",
+                    self.0.id, self.0.title, self.0.web_url
+                )))) // some_into(
+                     // SlackSectionBlock::new().with_text(md!())
+                     // )
+                     // some_into(
+                     //     // SlackSectionBlock::new().with_fields()
+                     // )
+            ])
     }
+}
+
+fn setup_logging(args: &CmdArgs) -> anyhow::Result<()> {
+    // First, setup LogTracer to catch events from the reqwest/gitlab crates
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| match args.verbose {
+            true => EnvFilter::try_new("debug"),
+            false => EnvFilter::try_new("info"),
+        })
+        .unwrap();
+    let fmtj = tracing_journald::layer().ok();
+    let fmtout = if fmtj.is_none() {
+        // Only setup the fmt layer if the journal connection doesn't work
+        Some(tracing_subscriber::fmt::layer().with_target(false))
+    } else {
+        // So the journald thing worked, therefore we don't setup the fmt output, to avoid duplicate logs
+        None
+    };
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmtout)
+        .with(fmtj)
+        .init();
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = CmdArgs::parse();
+    setup_logging(&args)?;
     let gitlab = Gitlab::builder(&args.gitlab_host, args.gitlab_token.expose_secret())
         .build_async()
         .await
         .context("unable to build gitlab API client")?;
     let now = Local::now();
     for mr in get_mrs(&args, &gitlab).await.unwrap() {
+        let _mr_span = info_span!("processing_merge_request").entered();
         if let Some(dwell) = args.min_dwell_secs {
             // Skip this MR and continue to next if the time since update is < dwell time
             if now.signed_duration_since(mr.updated_at).num_seconds() < dwell {
+                trace!(mr_id=?mr.id,updated_at=?mr.updated_at,dwell,"skipping due to insufficent dwell time");
                 continue;
             }
         }
@@ -98,17 +158,17 @@ async fn main() -> anyhow::Result<()> {
         );
         // print the message to stdout
         println!("{msg}");
-        if let Some(hook_url) = &args.slack_webhook_url {
+        if let Some(hook_url) = args.slack_webhook_url.clone() {
+            let _slack_span = info_span!("slack_webhook_post").entered();
             let client = SlackClient::new(SlackClientHyperConnector::new());
+
             client
                 .post_webhook_message(
-                    hook_url,
-                    &SlackApiPostWebhookMessageRequest::new(
-                        WrappedMR(mr).render_template(),
-                    ),
+                    &hook_url,
+                    &SlackApiPostWebhookMessageRequest::new(WrappedMR(mr).render_template()),
                 )
                 .await
-                .context("failed to send webhook")?;
+                .unwrap();
         }
     }
     Ok(())
